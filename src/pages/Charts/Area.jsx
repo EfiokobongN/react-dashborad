2af36caf5af7b6879816b7f117e10cfbfa4c5b54
EfiokobongN <mailpreciousnelson@gmail.com>
2024-02-06 20:41:08 +0000

    commit

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, DateTime, SplineAreaSeries, Legend } from '@syncfusion/ej2-react-charts';
 
-import { Header } from '../../components';
+import { ChartsHeader } from '../../components';
 import { areaCustomSeries, areaPrimaryXAxis, areaPrimaryYAxis } from '../../data/dummy';
 import { useStateContext } from '../../contexts/ContextProvider';
 
@@ -11,7 +11,7 @@ const Area = () => {
   const { currentMode } = useStateContext();
   return (
     <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
-      <Header category="Area" title="Inflation Rate in percentage" />
+      <ChartsHeader category="Area" title="Inflation Rate in percentage" />
       <div className="w-full">
       <ChartComponent
           id="charts"
